b40380c67b2171cdfc44954335587ee0280c2633
John M <johnm2@gmail.com>
2025-06-04 03:40:36 -0400

    euclid test

--- test2.f
+++ test2.f
@@ -1,11 +1,31 @@
-        program circle
-        real r, area, pi
-        parameter (pi = 3.14159)
-c This program reads a real number r and prints
-c the area of a circle with radius r.
-        write (*,*) 'Give radius r:'
-        read (*,*) r
-        area = pi*r*r
-        write (*,*) 'Area = ', area
-        stop
-        end
+C     euclid.f (FORTRAN 77)
+C     Find greatest common divisor using the Euclidean algorithm
+      PROGRAM EUCLID
+        PRINT *, 'A?'
+        READ *, NA
+        IF (NA.LE.0) THEN
+          PRINT *, 'A must be a positive integer.'
+          STOP
+        END IF
+        PRINT *, 'B?'
+        READ *, NB
+        IF (NB.LE.0) THEN
+          PRINT *, 'B must be a positive integer.'
+          STOP
+        END IF
+        PRINT *, 'The GCD of', NA, ' and', NB, ' is', NGCD(NA, NB), '.'
+        STOP
+      END
+
+      FUNCTION NGCD(NA, NB)
+        IA = NA
+        IB = NB
+    1   IF (IB.NE.0) THEN
+          ITEMP = IA
+          IA = IB
+          IB = MOD(ITEMP, IB)
+          GOTO 1
+        END IF
+        NGCD = IA
+        RETURN
+      END
